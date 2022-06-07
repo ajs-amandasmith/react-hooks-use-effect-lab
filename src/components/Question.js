aa9de89e682d2eabb95cf7eb297092bcdb6f881f
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-07 15:42:21 -0500

    Pass the tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      console.log(timeRemaining)
+      setTimeRemaining((timeRemaining) => timeRemaining - 1)
+      if (timeRemaining === 0) {
+        onAnswered(false)
+        setTimeRemaining(10)
+      }
+      
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(timerID)
+    }
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
